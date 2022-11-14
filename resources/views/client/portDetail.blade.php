6f60c9bebe8aded3f46ab61ac533c64b6420ce8c
sangtrancong171196@gmail.com <sang1996>
2022-11-14 11:47:18 +0700

    updateL

--- resources/views/client/portDetail.blade.php
+++ resources/views/client/portDetail.blade.php
@@ -13,7 +13,7 @@
     <meta property="twitter:image" content="{{ config('hostserver.domain') . 'storage/' . $port->image }}" />
     <meta property="twitter:image:url" content="{{ config('hostserver.domain') . 'storage/' . $port->image }}" />
     <meta property="twitter:description" content="{{ $port->short_content }}" />
-    {{-- <link rel="canonical" href="{{config('hostserver.domain')}}" /> --}}
+    <link rel="canonical" href="{{config('hostserver.domain') . 'storage/' . $port->image }}}}" />
 @endsection
 @section('title', 'Article')
 @section('content')
